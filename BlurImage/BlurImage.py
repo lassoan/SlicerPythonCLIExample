aeac283db7d3ab128468414daee38b769ef20546
Andras Lasso <lasso@cs.queensu.ca>
2019-07-04 15:17:44 -0400

    Add BlurImage example module

---
+++ BlurImage/BlurImage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python-real
+
+import SimpleITK as sitk
+import sys
+import os
+
+if len ( sys.argv ) < 4:
+    print( "Usage: SimpleGaussian <input> <sigma> <output>" )
+    sys.exit ( 1 )
+
+reader = sitk.ImageFileReader()
+reader.SetFileName(sys.argv[1])
+image = reader.Execute()
+
+pixelID = image.GetPixelID()
+
+gaussian = sitk.SmoothingRecursiveGaussianImageFilter()
+gaussian.SetSigma(float(sys.argv[2]))
+image = gaussian.Execute(image)
+
+caster = sitk.CastImageFilter()
+caster.SetOutputPixelType(pixelID)
+image = caster.Execute(image)
+
+writer = sitk.ImageFileWriter()
+writer.SetFileName (sys.argv[3])
+writer.Execute (image)
